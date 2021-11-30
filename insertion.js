f66b4116349878422f0b79272065f2d50125c286
Hp <alikoray@sabanciuniv.edu>
2021-12-01 01:14:24 +0300

    naber

--- insertion.js
+++ insertion.js
@@ -31,7 +31,12 @@ document.getElementById('ftype').onchange = function(e) {
 
     else if (value == "student")
     {
-
+        document.getElementById("student_idField").style.display = "block";
+        document.getElementById("student_cgpaField").style.display = "block";
+        //document.getElementById("student_enroll_yearField").style.display = "block";
+        document.getElementById("student_scholarshipField").style.display = "block";
+        document.getElementById("student_levelField").style.display = "block";
+        document.getElementById("student_advisor_idField").style.display = "block";
     }
 
     else if (value == "course")
@@ -40,14 +45,40 @@ document.getElementById('ftype').onchange = function(e) {
         document.getElementById("course_codeField").style.display = "block";
         document.getElementById("course_creditsField").style.display = "block";
     }
+    
 
-    else if (value = "club")
+    else if (value == "club")
     {
         document.getElementById("club_idField").style.display = "block";
         document.getElementById("club_member_countField").style.display = "block";
         document.getElementById("club_emailField").style.display = "block";
         document.getElementById("club_nameField").style.display = "block";
     }
+
+    else if(value == "faculty")
+    {
+        document.getElementById("faculty_idField").style.display = "block";
+        document.getElementById("faculty_nameField").style.display = "block";
+        document.getElementById("faculty_room_countField").style.display = "block";
+    }
+
+    else if(value == "program")
+    {
+        document.getElementById("program_idField").style.display = "block";
+        document.getElementById("program_nameField").style.display = "block";
+
+    }
+
+    else if (value=="timeslot")
+    {
+        document.getElementById("timeslot_idField").style.display = "block";
+        document.getElementById("timeslot_dayField").style.display = "block";
+        document.getElementById("timeslot_begin_timeField").style.display = "block";
+        document.getElementById("timeslot_end_timeField").style.display = "block";
+    }
+
+    
+
 
 }
 
